70b6ca8d8cc8b9446c5308544d6cc218a7aec05b
MrCodeGameandAnime <mrcodegameandanime@gmail.com>
2020-08-17 09:48:14 -0400

    learning about interfaces and companion objects

--- number_guessing_game/app/src/main/java/com/example/numberguessinggame/Exercises.kt
+++ number_guessing_game/app/src/main/java/com/example/numberguessinggame/Exercises.kt
@@ -2,44 +2,33 @@ package com.example.numberguessinggame
 
 import android.util.Log
 
-
 fun main() {
     var gameUser = GameUser("Jack","Black","0983451289",
         4,"03/22/76",60.9) // Exercise 12
-    Log.d("CONST", "onCreate: " +Window.NUMBER_OF_TABS+"\n"+Window.PREFS_FILE+"\n"+Window.WINDOW_TITLE) //Exercise 17
+    println("onCreate: " +Window.NUMBER_OF_TABS+"\n"+Window.PREFS_FILE+"\n"+Window.WINDOW_TITLE) //Exercise 17
 
     var triangleOne = Triangle(2,2,4) //Exercise 19
     val numberOfCorners = Triangle.NUMBER_OF_CORNERS // Exercise 19
+
+    val collector = Collector()
+    val triangle:GraphicsObject = GraphicTriangle()
+    collector.add(triangle)
 }
 
 class Person(val firstName:String,val lastName:String,val ssn:String = " ",val dateOfBirth:String,val gender:Char) {
     // Exercise 8,13, and 15
     constructor(firstName: String,lastName: String,ssn: String,gender: Char) :
-            this(firstName = firstName,
-                lastName = lastName,
-                ssn = ssn,
-                gender = gender,
-                dateOfBirth = "0000-00-00")
-
+            this(firstName = firstName,lastName = lastName,ssn = ssn,gender = gender,dateOfBirth = "0000-00-00")
 }
-class GameUser(val firstName:String,
-               val lastName:String,
-               val userName:String,
-               val registrationNumber:Int,
-               val birthday:String = "1910-06-07", // Exercise 10 and Exercise 14
-               val userRank:Double = 0.0) {
+class GameUser(val firstName:String,val lastName:String,val userName:String, val registrationNumber:Int,
+               val birthday:String = "1910-06-07",val userRank:Double = 0.0) { // Exercise 10 and Exercise 14
     // Secondary constructor
     constructor(firstName: String,lastName: String,userName: String,registrationNumber: Int) :
-            this(firstName = firstName,
-                lastName = lastName,
-                userName = userName,
-                registrationNumber = registrationNumber,
-                birthday = "",
-                userRank = 0.0)
+            this(firstName = firstName,lastName = lastName,userName = userName,registrationNumber = registrationNumber,
+                birthday = "",userRank = 0.0)
 }
 // Exercise 18
 class Triangle(firstSideLength:Int,secondSideLength:Int,thirdSideLength:Int) {
-
     companion object {
         const val NUMBER_OF_CORNERS = 3
     }
@@ -47,6 +36,83 @@ class Triangle(firstSideLength:Int,secondSideLength:Int,thirdSideLength:Int) {
     fun info() {
         Log.d("EXER", "info: $NUMBER_OF_CORNERS")
     }
+}
+
+class Collector {
+
+    fun add(graphics:GraphicsObject) {
+        println("Collector.add()")
+        println("Number of corners: "+ graphics.numberOfCorners())
+
+        println("Color: " + graphics.fillColor())
+        println("Coordinates of x and y" +graphics.coordsOf(1))
+    }
+}
+
+class GraphicTriangle: GraphicsObject {
+    override fun numberOfCorners(): Int {
+        return 3
+    }
+
+    override fun coordsOf(index: Int): Pair<Double, Double> {
+        return when(index) {
+            0 -> Pair(-1.0,0.0)
+            1 -> Pair(1.0,0.0)
+            2 -> Pair(0.0,1.0)
+            else -> throw RuntimeException("Index ${index} out of bounds")
+        }
+    }
+
+    override fun fillColor(): String {
+        return "red"
+    }
+}
+
+class GraphicTriangleAlt: GraphicsObject {
+    override fun numberOfCorners() = 3
+    override fun fillColor() = "red"
+    override fun coordsOf(index:Int) =
+        when(index) {
+            0 -> Pair(-1.0,0.0)
+            1 -> Pair(1.0,0.0)
+            2 -> Pair(0.0,1.0)
+            else -> throw RuntimeException(
+                "Index ${index} out of bounds")
+        }
+}
+
+class Electron: ElementaryParticle {
+    // Exercise 20
+    override fun mass() = 9.11e-31
+    override fun charge() = -1.0
+    override fun spin() = 0.5
+}
+
+class Proton: ElementaryParticle {
+    override fun mass(): Double {
+        return 1.67e-27
+    }
+
+    override fun charge(): Double {
+        return 0.5
+    }
+
+    override fun spin(): Double {
+        return 0.5
+    }
+}
+
+interface GraphicsObject {
+    fun numberOfCorners(): Int
+    fun coordsOf(index:Int): Pair<Double,Double>// Represents x and y coordinates of a point
+    fun fillColor(): String
+}
+
+interface ElementaryParticle {
+    // Exercise 20
+    fun mass(): Double
+    fun charge(): Double
+    fun spin(): Double
 }
 
 val firstPerson = Person("Jack","Black","0983451289","03/22/76",'M') // Exercise 9
@@ -58,16 +124,7 @@ val secondPerson = Person(
     gender = 'M') // Exercise 11
 val thirdPerson = Person("Tito","Ortiz","00000",'M') // Exercise 15
 
-val gameUserOne = GameUser(
-    // Named constructor parameter instantiation is more explicit and human readable
-    firstName = "Saya",
-    lastName = "Diva",
-    birthday = "200BC",
-    userName = "QueenOfTheDamned",
-    registrationNumber = 345,
-    userRank = 5.6)
-val gameUserTwo = GameUser(
-    firstName = "Heath",
-    lastName = "Ledger",
-    userName = "TheJoker",
-    registrationNumber = 777)
+// Named constructor parameter instantiation is more explicit and human readable
+val gameUserOne = GameUser(firstName = "Saya",lastName = "Diva",birthday = "200BC",
+    userName = "QueenOfTheDamned",registrationNumber = 345,userRank = 5.6)
+val gameUserTwo = GameUser(firstName = "Heath",lastName = "Ledger",userName = "TheJoker",registrationNumber = 777)
